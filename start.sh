dd05b25e9d2249b7ab3172d38d7faac18c27f3f2
NoPref <149392305+NoPref@users.noreply.github.com>
2024-12-22 21:00:07 +0300

    start

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 # Start ZeroTier service
-zerotier-one &
+zerotier-one -d
 
 # Wait for ZeroTier to initialize
 sleep 5
 
-# Join your network
+# Join the network
 zerotier-cli join 48d6023c46b63da5
 
-# Keep the container running
+# Enable relay mode
+zerotier-cli set 48d6023c46b63da5 allowManaged=0
+zerotier-cli set 48d6023c46b63da5 allowGlobal=0
+zerotier-cli set 48d6023c46b63da5 allowDefault=0
+
+# Keep container running
 tail -f /dev/null
